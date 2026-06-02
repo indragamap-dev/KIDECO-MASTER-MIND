e3ba6c486b9ac03d6ee6f54ebcedf68a26db97ac
indragamap-dev <dramacrack@gmail.com>
2026-06-02 21:49:23 +0800

    Build KMM Engine

--- app.js
+++ app.js
@@ -1,1 +1,39 @@
-console.log("KIDECO MASTER MIND Loaded");
+function startStudy(){
+
+document
+.getElementById("content")
+.innerHTML=
+
+`
+<h2>
+Study Mode
+</h2>
+
+<p>
+Modul pembelajaran
+akan ditampilkan
+di sini.
+</p>
+`;
+
+}
+
+function startQuiz(){
+
+document
+.getElementById("content")
+.innerHTML=
+
+`
+<h2>
+Quiz Mode
+</h2>
+
+<p>
+Engine Quiz akan
+dibangun pada
+Tahap 3.
+</p>
+`;
+
+}
