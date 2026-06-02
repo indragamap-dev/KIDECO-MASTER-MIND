32d2c20af94fc0accf42e12f9160be2e08db2cae
indragamap-dev <dramacrack@gmail.com>
2026-06-02 21:56:35 +0800

    Activate Study Mode

--- app.js
+++ app.js
@@ -1,33 +1,21 @@
 function startStudy(){
 
 const topic =
-document
-.getElementById("topic")
-.value;
+document.getElementById("topic").value;
 
 const data =
 knowledgeBase[topic];
 
-document
-.getElementById("content")
-.innerHTML=
-
-`
-
-<h2>
-${data.title}
-</h2>
+document.getElementById("content").innerHTML = `
+<h2>${data.title}</h2>
 
 <pre style="
 white-space:pre-wrap;
 font-size:16px;
 line-height:1.6;
 ">
-
 ${data.summary}
-
 </pre>
-
 `;
 
 }
